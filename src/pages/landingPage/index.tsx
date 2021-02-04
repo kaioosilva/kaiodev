15f96380ac3b213ae7ed3431f11dfcbe8122e706
Kaio <kaioo_silva@outlook.com>
2021-02-04 18:02:00 +0000

    Add link router dom on main page
    Signed-off-by: Kaio <kaioo_silva@outlook.com>

--- src/pages/landingPage/index.tsx
+++ src/pages/landingPage/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef, useCallback, useEffect } from "react";
+import { Link } from 'react-router-dom';
 import {
   AiOutlineArrowRight,
   AiOutlineBulb,
@@ -551,14 +552,13 @@ const LandingPage: React.FC = () => {
                     <div className="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7">
                       <p>Want to dig deeper into Gobarber?</p>
                       <p>
-                        <a
-                          href="https://github.com/kaioosilva/gobarber-web"
+                        <Link
+                          to="https://github.com/kaioosilva/gobarber-web"
                           className="text-primary hover:text-primary-dark"
-                          title="Gobarbar project on Github"
                         >
                           {" "}
                           Go to Github &rarr;{" "}
-                        </a>
+                        </Link>
                       </p>
                     </div>
                   </div>
@@ -629,14 +629,13 @@ const LandingPage: React.FC = () => {
                     <div className="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7">
                       <p>Want to dig deeper into Zoom-clone?</p>
                       <p>
-                        <a
-                          href="https://github.com/kaioosilva/zoom-clone"
+                        <Link
+                          to="https://github.com/kaioosilva/zoom-clone"
                           className="text-primary hover:text-primary-dark"
-                          title="Zoom-clone project on github"
                         >
                           {" "}
                           Go to Github &rarr;{" "}
-                        </a>
+                        </Link>
                       </p>
                     </div>
                   </div>
@@ -707,14 +706,13 @@ const LandingPage: React.FC = () => {
                     <div className="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7">
                       <p>Want to dig deeper into Github explorer?</p>
                       <p>
-                        <a
-                          href="https://github.com/kaioosilva/github-explorer"
+                        <Link
+                          to="https://github.com/kaioosilva/github-explorer"
                           className="text-primary hover:text-primary-dark"
-                          title="Github explorer project on github"
                         >
                           {" "}
                           Go to Github &rarr;{" "}
-                        </a>
+                        </Link>
                       </p>
                     </div>
                   </div>
@@ -838,10 +836,10 @@ const LandingPage: React.FC = () => {
           </div>
 
           <div className="flex flex-row justify-center items-center w-full mb-10 mt-8">
-            <a
+            <Link
               onMouseEnter={() => setIconsLinkedinReactions(true)}
               onMouseLeave={() => setIconsLinkedinReactions(false)}
-              href="https://www.linkedin.com/in/kaio-oliveira-silva-54275b57/"
+              to="https://www.linkedin.com/in/kaio-oliveira-silva-54275b57/"
               title="Kaio's linkedin profile page"
               className=" flex items-center justify-center w-14 h-14 bg-gray-800 text-white hover:bg-primary cursor-pointer rounded"
             >
@@ -852,11 +850,11 @@ const LandingPage: React.FC = () => {
               ) : (
                 <ImLinkedin2 size={25} />
               )}
-            </a>
-            <a
+            </Link>
+            <Link
               onMouseEnter={() => setIconsGithubReactions(true)}
               onMouseLeave={() => setIconsGithubReactions(false)}
-              href="https://github.com/kaioosilva"
+              to="https://github.com/kaioosilva"
               title="Kaio's github profile page"
               className="flex items-center justify-center w-14 h-14 bg-gray-800 text-white hover:bg-primary cursor-pointer ml-4 rounded"
             >
@@ -867,11 +865,11 @@ const LandingPage: React.FC = () => {
               ) : (
                 <ImGithub size={25} />
               )}
-            </a>
-            <a
+            </Link>
+            <Link
               onMouseEnter={() => setIconsInstagramReactions(true)}
               onMouseLeave={() => setIconsInstagramReactions(false)}
-              href="https://www.instagram.com/kaioo/"
+              to="https://www.instagram.com/kaioo/"
               title="Kaio's instagram profile page"
               className="flex items-center justify-center w-14 h-14 bg-gray-800 text-white hover:bg-primary cursor-pointer ml-4 rounded"
             >
@@ -882,7 +880,7 @@ const LandingPage: React.FC = () => {
               ) : (
                 <AiOutlineInstagram size={25} />
               )}
-            </a>
+            </Link>
           </div>
 
           <div className="flex justify-center items-center w-full text-gray-700 text-sm">
